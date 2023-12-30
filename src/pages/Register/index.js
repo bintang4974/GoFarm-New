03d29cc3aa48ad583a93f1acbed87213a99abfd8
Bagasadhe27 <bagas.adhe.21@student.is.ittelkom-sby.ac.id>
2023-12-30 15:09:17 +0700

    Register Screen

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -13,18 +13,22 @@ const Login = ({navigation}) => {
                 <Box backgroundColor={colors.white} p={5} mx={8} borderRadius={20} shadow={5}>
                     <HStack space={2} alignItems={"center"} justifyContent={"center"}>
                         <Image source={Logo} height={50} width={50} alt='logo' />
-                        <Heading fontWeight={"bold"} size={"2xl"} color={colors.primary}>Login</Heading>
+                        <Heading fontWeight={"bold"} size={"2xl"} color={colors.primary}>Register</Heading>
                     </HStack>
                     <Gap height={20} />
+                    <Input label="Name" />
+                    <Input label="Deskription" secureTextEntry />
                     <Input label="Email" />
                     <Input label="Password" secureTextEntry />
+                    <Input label="Phone" />
+                    <Input label="Address" secureTextEntry />
                     <Gap height={20} />
                     <Box mx={10} shadow={5}>
-                        <Button title="Login" type="text" padding={10} onPress={() => navigation.navigate('MainApp')} />
+                        <Button title="Simpan" type="text" padding={10} onPress={() => navigation.navigate('MainApp')} />
                     </Box>
                     <Gap height={20} />
                     <TouchableOpacity>
-                    <Text fontWeight={"semibold"} fontSize={"md"} color={colors.primary} textAlign={"center"} onPress={() => navigation.navigate('Register')}>Register Account?</Text>
+                    <Text fontWeight={"semibold"} fontSize={"md"} color={colors.primary} textAlign={"center"} onPress={() => navigation.navigate('Login')}>Kembali?</Text>
                     </TouchableOpacity>
                 </Box>
             </Box>
