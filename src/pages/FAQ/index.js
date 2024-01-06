ce9710b90ce53a628b8f8672c6e1ea75e4b7f9be
Anvy123 <bintangramadhan4974@gmail.com>
2024-01-06 18:04:18 +0700

    fixing button faq

--- src/pages/FAQ/index.js
+++ src/pages/FAQ/index.js
@@ -1,8 +1,8 @@
-import { Box, Button, Center, Heading, ScrollView, Text } from 'native-base';
+import { Box, Center, Heading, ScrollView, Text } from 'native-base';
 import React, { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Gap, ListCategory, ListProducts } from '../../components';
+import { Button, Gap, ListCategory, ListProducts } from '../../components';
 import { colors } from '../../utils';
 
 const FAQ = ({ navigation }) => {
@@ -24,9 +24,9 @@ const FAQ = ({ navigation }) => {
   return (
     <>
       <SafeAreaView>
-          <Center>
-            <Box width='80%' p={4} borderWidth={1} borderRadius={8} borderColor={colors.primary} shadow={2}>
-              <ScrollView>
+        <Center>
+          <Box width='80%' p={4} borderWidth={1} borderRadius={8} borderColor={colors.primary} shadow={2}>
+            <ScrollView>
               {faqData.map((item, index) => (
                 <Box>
                   <Heading size="md" mb={2}>
@@ -35,12 +35,22 @@ const FAQ = ({ navigation }) => {
                   <Text>{item.answer}</Text>
                 </Box>
               ))}
-              </ScrollView>
-            </Box>
-          </Center>
+            </ScrollView>
+          </Box>
+        </Center>
         <Gap height={10} />
-        <Box mx={10} shadow={5} >
-          <Button borderRadius={8} borderWidth={2} borderColor={colors.primary} shadow={7} backgroundColor={colors.primary} title="Saya Paham!" type="text" padding={7} onPress={() => navigation.navigate('MainApp')} color={colors.primary} />
+        <Box mx={10} shadow={5}>
+          <Button
+            type="textIcon"
+            title="Back"
+            icon="back"
+            padding={15}
+            fontSize={18}
+            backgroundColor={colors.primary}
+            fontColor={colors.white}
+            onPress={navigation.navigate('MainApp')}
+          />
+          {/* <Button borderRadius={8} borderWidth={2} borderColor={colors.primary} shadow={7} backgroundColor={colors.primary} title="Saya Paham!" type="text" padding={7} onPress={() => navigation.navigate('MainApp')} color={colors.primary} /> */}
         </Box>
       </SafeAreaView>
     </>
