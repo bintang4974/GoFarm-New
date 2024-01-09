1fd9cf896f0dfaf14db56f999770379a9ad86ed3
Anvy123 <bintangramadhan4974@gmail.com>
2024-01-09 15:40:21 +0700

    fixing assets

---
+++ assets/icons/index.js
@@ -0,0 +1,6 @@
+import IconHomeActive from './home-active.svg';
+import IconHome from './home.svg';
+import IconCart from './cart.svg'
+import IconCart2 from './cart2.svg'
+
+export {IconHomeActive, IconHome, IconCart, IconCart2}
---
+++ assets/images/index.js
@@ -0,0 +1,10 @@
+import Slider1 from './slider1.png';
+import Product1 from './product1.png';
+import Product2 from './product2.png';
+import Product3 from './product3.png';
+import Product4 from './product4.png';
+import Logo from './logo.jpg'
+import Avatar from './avatar.png';
+import Onboardings from './Onboardings.png';
+
+export { Slider1, Product1, Product2, Product3, Product4, Logo, Avatar, Onboardings }
